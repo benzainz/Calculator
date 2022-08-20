3d9bee4836f31d152bd42bb7831f2eee935fb071
benjamin sainz <hueroacapulco@gmail.com>
2022-08-20 17:57:32 -0500

    Build functions to handle keyboard numbers

--- addStyles.js
+++ addStyles.js
@@ -32,7 +32,9 @@ buttons.forEach((btn) => {
 function addClasses(button){
     const btnNumberClass = "hover:bg-green-500 border-green-500"
     const btnClearClass = "hover:bg-red-500 border-red-500"
-    const btnOpClass = "hover:bg-blur-500 border-blue-500"
+    const btnOpClass = "hover:bg-blue-500 border-blue-500"
+    const btnEqClass = "hover:bg-orange-500 border-orange-500"
+    const btnDecClass= "hover:bg-yellow-500 border-yellow-500"
 
     if(button.classList.value === "number") {
         btnNumberClass.split(" ").forEach((cls) => button.classList.add(cls));
@@ -40,8 +42,12 @@ function addClasses(button){
         btnOpClass.split(" ").forEach((cls) => button.classList.add(cls));
     }else if (button.classList.value === "clear"){
         btnClearClass.split(" ").forEach((cls) => button.classList.add(cls));
+    }else if (button.classList.value === "equal"){
+        btnEqClass.split(" ").forEach((cls) => button.classList.add(cls));
+    }else if (button.classList.value === "decimal"){
+        btnDecClass.split(" ").forEach((cls) => button.classList.add(cls));
     }
     const baseClass = 
-    "flex w12 h-12 justify-center items-center m-1 font-bold py-2 px-2 border hover:border-transparent rounded text-white border-2 text-2xl"
+    "flex w-12 h-12 justify-center items-center m-1 font-bold py-2 px-2 border hover:border-transparent rounded text-white border-2 text-2xl"
     baseClass.split(" ").forEach((cls) => button.classList.add(cls));
 }
--- main.js
+++ main.js
@@ -5,10 +5,21 @@ let operator = ""
 // select items from DOM to manipulate 
 const currentDisplayNumber = document.querySelector(".currentNumber")
 const previousDisplayNumber = document.querySelector(".previousNumber")
-const equal = document.querySelector(".equal")
-equal.addEventListener("click", calculate)
-const decimal = document.querySelector(".decimal")
+window.addEventListener("keydown", handleKeyPress)
+
+const equal = document.querySelector(".equal");
+equal.addEventListener("click", ()=> {
+    if (currentNum != "" && previousNum != ""){
+        calculate();
+    }
+})
+
+const decimal = document.querySelector(".decimal");
+decimal.addEventListener("click", () => {
+    addDecimal();
+} )
 const clear = document.querySelector(".clear")
+clear.addEventListener('click', clearCalculator);
 
 const numberButtons = document.querySelectorAll(".number")
 const operators = document.querySelectorAll(".operator")
@@ -22,6 +33,10 @@ numberButtons.forEach(btn => {
 })
 
 function handleNumber(number){
+    if (previousNum !== "" && currentNum !== "" && operator === ""){
+        previousNum = ""
+        currentDisplayNumber.textContent = currentNum;
+    }
     if (currentNum.length <= 11){
         currentNum += number;
         currentDisplayNumber.textContent = currentNum;
@@ -34,17 +49,27 @@ operators.forEach(btn => {
 })
 
 function handleOperators(op){
-    //" " = (/,*,-,+) so operator va a tener el signo q le pases
-    operator = op;
-    //" " = " "
-    previousNum = currentNum;
-    //pasa a previous box lo q tenia la current box 
-    previousDisplayNumber.textContent = previousNum + " " + operator;
-    // se resetea la current number box 
-    currentNum = "";
-    currentDisplayNumber.textContent = "";
+    if (previousNum === ""){
+        previousNum = currentNum;
+        operatorCheck(op);
+    }else if (currentNum === ""){
+        operatorCheck(op)
+    }else {
+        calculate();
+        operator = op;
+        currentDisplayNumber.textContent = "0"
+        previousDisplayNumber.textContent = previousNum + " " + operator;
+    }
     
 }
+function operatorCheck(text){
+    operator = text;
+    previousDisplayNumber.textContent = previousNum + " " + operator;
+    currentDisplayNumber.textContent = "0";
+    currentNum = "";
+}
+
+
 //function to handle = 
 function calculate (){
     previousNum = Number(previousNum);
@@ -60,9 +85,81 @@ function calculate (){
         previousNum = previousNum * currentNum;
     }
     else if (operator === "/"){
+        if (currentNum <= 0){
+            previousNum = "Error";
+            displayResult();
+            return;
+        }
         previousNum = previousNum / currentNum;
     }
-    previousDisplayNumber.textContent = "";
-    currentDisplayNumber.textContent = previousNum;
+    previousNum = roundNumber(previousNum);
+    previousNum = previousNum.toString();
+    displayResult();
+}
 
+function roundNumber(num){
+    return Math.round(num * 1000000) / 1000000;
+}
+
+function displayResult(){
+    
+    if (previousNum.length<=11){
+        currentDisplayNumber.textContent = previousNum;
+    }
+    else {
+        currentDisplayNumber.textContent = previousNum.slice(0,11) + "...";
+    }
+    previousDisplayNumber.textContent= "";
+    operator = "";
+    currentNum = "";
+}
+function clearCalculator(){
+    currentNum = ""
+    previousNum = ""
+    operator = ""
+    currentDisplayNumber.textContent = "0"
+    previousDisplayNumber.textContent=""
+}
+function addDecimal (){
+    if(!currentNum.includes(".")){
+        currentNum += ".";
+        currentDisplayNumber.textContent = currentNum;
+    }
+
+}
+function handleKeyPress(e){
+    e.preventDefault();
+    if (e.key >= 0 && e.key <= 9){
+        handleNumber(e.key);
+    }
+    if (e.key === "Enter" || 
+    (e.key === "=" && currentNum != "" &&  previousNum != "")){
+        calculate();
+    }
+    if (e.key === "+" || e.key === "-" || e.key === "/"){
+        handleOperators(e.key);
+    }
+    if (e.key === "*"){
+        handleOperators("x")
+    }
+    if (e.key === "."){
+        addDecimal();
+    }
+    if (e.key === "Backspace"){
+        handleDelete();
+    }
+}
+
+function handleDelete(){
+    if (currentNum != ""){
+        currentNum = currentNum.slice(0,-1);
+        currentDisplayNumber.textContent = currentNum;
+        if(currentNum === ""){
+            currentDisplayNumber.textContent = "0";
+        }
+    }
+    if (currentNum === "" && previousNum !== "" && operator === ""){
+        previousNum = previousNum.slice(0,-1);
+        currentDisplayNumber.textContent = previousNum;
+    }
 }
